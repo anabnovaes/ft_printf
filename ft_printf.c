442e1860ce180fce474784bce6547eed359d876e
Ana Paula Novaes <ana-paula1503@hotmail.com>
2021-06-08 18:59:23 -0300

    adicionando estrutura do projeto

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 11:17:12 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/05/23 10:41:17 by apaula-b         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:18:58 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,26 @@
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
+	size_t	counter;
+	size_t	len;
 
 	va_start(args, format);
+	len = 0;
+	counter = 0;
+	while (format[counter])
+	{
+		if (format[counter] == "%" && format[counter + 1] == "%")
+		{
+			len += ft_putchar_fd(format[counter, 1]);
+			counter ++;
+		}
+		else if (format[counter] == "%")
+		{
+			len += get_flags_and_precision(format[counter], args);
+		}
+		else
+			len += ft_putchar_fd(format[counter, 1]);
+		counter++;
+	}
+	return (len);
 }
--- utils/ft_putchar_fd.c
+++ utils/ft_putchar_fd.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apaula-b <apaula-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/21 11:18:13 by apaula-b          #+#    #+#             */
-/*   Updated: 2021/06/06 17:27:54 by apaula-b         ###   ########.fr       */
+/*   Created: 2021/02/21 17:30:47 by apaula-b          #+#    #+#             */
+/*   Updated: 2021/02/21 17:46:49 by apaula-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "libft.h"
 
-# include <stdarg.h>
-# include <stdlib.h>
-# include <stddef.h>
-# include <unistd.h>
-
-int		ft_printf(const char *format, ...);
-
-size_t	get_flags_and_precision( char position, va_list args);
-
-#endif
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
